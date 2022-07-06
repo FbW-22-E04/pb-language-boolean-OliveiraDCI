4c179ec876841f61da4a73664afb5d55ff6c1966
OliveiraDCI <oliveira.sein.dci@gmail.com>
2022-07-06 13:39:22 +0200

    Exercise concluded.

---
+++ solution.js
@@ -0,0 +1,20 @@
+// 1
+console.log(3 == "3");
+console.log(3 === "3");
+console.log(`1. To compare VALUE, we can use the equality operatos == or ===`);
+console.log(
+  `1.7 is correct, we must use the strict equality operator to compare both value and type.`
+);
+console.log(`1.9 The = operator is used to assign a value to a variable.`);
+
+// 2
+let myVar = true;
+console.log(`2. ${!myVar ? "good morning" : "good evening"}`);
+
+// 3
+let firstName = "" || "John" || 0;
+let emptyStr = "" && false && "Hello World";
+let zero = "" || false || 0;
+let seven = 75 && "nine" && 7;
+
+console.log(firstName, zero, emptyStr, seven);
